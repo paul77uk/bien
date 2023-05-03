0658cc53cbf1f8d41830d5bd08767e375c17d768
Paul Vickers <paul77uk@gmail.com>
2023-05-03 20:30:17 +0100

    added ambiance and SEO

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -50,6 +50,6 @@ class ReviewsController < ApplicationController
   end
 
   def form_params
-    params.require(:review).permit(:title, :body, :score)
+    params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance)
   end
 end
---
+++ db/migrate/20230503190214_add_info_to_reviews.rb
@@ -0,0 +1,6 @@
+class AddInfoToReviews < ActiveRecord::Migration[7.0]
+  def change
+    add_column :reviews, :phone_number, :string
+    add_column :reviews, :ambiance, :string
+  end
+end
